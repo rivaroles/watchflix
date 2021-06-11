1f7e5b45e28a45feb0afe3064a76a7875186fcae
riva <rivaroles98@gmail.com>
2021-06-11 20:16:13 -0300

    adding plugins (vuetify, axios)

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
-  <div id="app">
-    <Home />
-  </div>
+  <v-app>
+    <toolbar />
+    <v-main>
+      <v-container fluid fill-height>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import Home from "./views/Home.vue";
+import Toolbar from "./components/Toolbar";
 
 export default {
   name: "App",
+
   components: {
-    Home,
+    Toolbar,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
